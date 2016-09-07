c115112c3cb260ea506a28c2d9f5a9102ba12384
mishakuzma <mishakuzma@gmail.com>
2016-09-06 22:06:06 -0400

    Straightened out form, figured out api for wikipedia
    -HTML edited so that the front end is straight
    -New function added so that wikipedia should access api correctly.
    -Must edit so that each entry is accessed

---
+++ wikipediaAPI.js
@@ -0,0 +1,7 @@
+
+// function will display the data from a wiki page. (Put a new function to put data in rows)
+function displayWiki(wikiJson) {
+  var pagesArray = wikiJson["query"]["pageids"];
+  for(var i = 0; i < pagesArray.length; i++){
+    var title = wikiJson["query"]  
+  }
